00673b470b892c3f45471176903ee8825dd66625
pandelegeorge@telacad.ro <Pandele#Telacad3901>
2018-09-06 20:51:50 +0300

    adding url as /id instead of ?id

--- shop/urls.py
+++ shop/urls.py
@@ -6,9 +6,9 @@ from . import views
 app_name = 'shop'
 
 urlpatterns = [
-    url(r'^(?P<productid>\d+)/$', views.product_detail, name='product_detail'),
-    re_path('^$', views.product_list, name='product_list'),
-    #url(r'produs/', views.product_detail, name='product_details')
+    url(r'^category/(?P<categoryid>\d+)/$', views.category_prodcut_list, name='product_list_by_category'),
+    url(r'^product/(?P<productid>\d+)/$', views.product_detail, name='product_detail'),
+    re_path('^$', views.category_prodcut_list, name='product_list'),
     #url(r'^(?P<categoryid>\d+)/$', views.product_list, name='product_list_by_category'),
     #url(r'^(?P<productid>\d+)/$', views.product_detail, name='product_detail'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -11,6 +11,14 @@ def product_list(request):
     }
     return render(request, 'list.html', context)
 
+def category_prodcut_list(request, categoryid = 1 ):
+    categories = Category.objects.all()
+    products = Product.objects.filter(category=categoryid)
+    context = {
+        'products': products,
+        'categories': categories
+    }
+    return render(request, 'categoryproductlist.html', context)
 def product_detail(request, productid):
     id = productid
     produs = Product.objects.get(pk=int(id))
